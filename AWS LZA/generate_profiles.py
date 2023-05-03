daf127acbb84af892fd0ef4030fc2eeb4cc9bb37
Hicham El Alaoui <hicham.elalaoui-ext@mcn.gouv.qc.ca>
2023-05-03 13:53:54 -0400

    Update 2023-05-03

--- AWS LZA/generate_profiles.py
+++ AWS LZA/generate_profiles.py
@@ -1,7 +1,7 @@
 accounts = {
     "222222222222": "Journalisation",
     "111111111111": "Securite",
- 
+}
 
 for account_id, account_name in accounts.items():
     print(f"[profile {account_name}]\nrole_arn = arn:aws:iam::{account_id}:role/OrganizationAccountAccessRole\ncredential_source = EcsContainer\n\n")
