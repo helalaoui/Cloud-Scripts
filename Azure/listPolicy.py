5ae73b3d2055653cfa3bdfac2dceb3db25456159
Hicham El Alaoui <hicham.elalaoui-ext@mcn.gouv.qc.ca>
2023-06-29 12:09:43 -0400

    Version 1.6.2 - 2023-06-28

--- Azure/listPolicy.py
+++ Azure/listPolicy.py
@@ -2,5 +2,5 @@ from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import PolicyClient
 
 # Acquire a credential object using CLI-based authentication.
-credential = AzureCliCredential(exclude_shared_token_cache_credential=True)
+credential = DefaultAzureCredential(exclude_shared_token_cache_credential=True)
 
