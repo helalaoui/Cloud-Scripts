7ff30afa4d0b5ce8eb4d80199e762b725de825c1
Hicham El Alaoui <56451184+helalaoui@users.noreply.github.com>
2024-02-28 20:27:39 -0500

    Delete Azure/listPolicy.py

--- Azure/listPolicy.py
+++
@@ -1,6 +0,0 @@
-from azure.identity import DefaultAzureCredential
-from azure.mgmt.resource import PolicyClient
-
-# Acquire a credential object using CLI-based authentication.
-credential = DefaultAzureCredential(exclude_shared_token_cache_credential=True)
-
